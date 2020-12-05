02728a87a1c146465b93f5ba86109b41a6b99dd0
quocthi26tran <73685152+quocthi26tran@users.noreply.github.com>
2020-12-05 15:25:27 +0700

    Add files via upload

---
+++ QLBanMayTinh1/QLBanMayTinh/FormHang.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using QLBanMayTinh.Class;
+
+namespace QLBanMayTinh
+{
+    public partial class FormHang : Form
+    {
+        private DataTable tblHang;
+        public FormHang()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHang_Load(object sender, EventArgs e)
+        {
+            txtMaHang.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = false;
+            LoadDataGridView();
+        }
+
+        private void LoadDataGridView()
+        {
+            string sql;
+            sql = "SELECT * from TblHang";
+            tblHang = Function.GetDataToTable(sql);
+            dgvHang.DataSource = tblHang;
+            dgvHang.Columns[0].HeaderText = "Mã hàng";
+            dgvHang.Columns[1].HeaderText = "Tên hàng";
+            dgvHang.Columns[2].HeaderText = "Số lượng";
+            dgvHang.Columns[3].HeaderText = "Giá bán";
+            dgvHang.Columns[4].HeaderText = "Ảnh";
+            dgvHang.Columns[5].HeaderText = "Ghi chú";
+            dgvHang.Columns[0].Width = 80;
+            dgvHang.Columns[1].Width = 140;
+            dgvHang.Columns[2].Width = 80;
+            dgvHang.Columns[3].Width = 100;
+            dgvHang.Columns[4].Width = 200;
+            dgvHang.Columns[5].Width = 300;
+            dgvHang.AllowUserToAddRows = false;
+            dgvHang.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void ResetValues()
+        {
+            txtMaHang.Text = "";
+            txtTenHang.Text = "";
+            txtSoLuong.Text = "";
+            txtGiaBan.Text = "";
+            txtSoLuong.Enabled = true;
+            txtGiaBan.Enabled = false;
+            txtAnh.Text = "";
+            txtGhiChu.Text = "";
+        }
+
+        private void dgvHang_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (btnThem.Enabled == false)
+            {
+                MessageBox.Show("Đang ở chế độ thêm mới!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaHang.Focus();
+                return;
+            }
+            if (tblHang.Rows.Count == 0)
+            {
+                MessageBox.Show("Không có dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            txtMaHang.Text = dgvHang.CurrentRow.Cells["MaHang"].Value.ToString();
+            txtTenHang.Text = dgvHang.CurrentRow.Cells["TenHang"].Value.ToString();
+            txtSoLuong.Text = dgvHang.CurrentRow.Cells["SoLuong"].Value.ToString();
+            txtGiaBan.Text = dgvHang.CurrentRow.Cells["GiaBan"].Value.ToString();
+
+            sql = "SELECT Anh FROM TblHang WHERE MaHang=N'" + txtMaHang.Text + "'";
+            txtAnh.Text = Function.GetFieldValues(sql);
+            sql = "SELECT GhiChu FROM TblHang WHERE MaHang = N'" + txtMaHang.Text + "'";
+            txtGhiChu.Text = Function.GetFieldValues(sql);
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
+            btnBoQua.Enabled = true;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
+            btnBoQua.Enabled = true;
+            btnLuu.Enabled = true;
+            btnThem.Enabled = false;
+            ResetValues();
+            txtMaHang.Enabled = true;
+            txtMaHang.Focus();
+            txtSoLuong.Enabled = true;
+            txtGiaBan.Enabled = true;
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            string sql, gt;
+            if (txtMaHang.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập mã hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaHang.Focus();
+                return;
+            }
+            if (txtTenHang.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtTenHang.Focus();
+                return;
+            }
+            if (txtSoLuong.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập số lượng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtSoLuong.Focus();
+                return;
+            }
+            if (txtGiaBan.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập giá bán", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtGiaBan.Focus();
+                return;
+            }
+            if (txtAnh.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập địa chỉ ảnh", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtAnh.Focus();
+                return;
+            }
+
+
+
+            sql = "SELECT MaHang FROM TblHang WHERE MaHang=N'" + txtMaHang.Text.Trim() + "'";
+            if (Function.CheckKey(sql))
+            {
+                MessageBox.Show("Mã hàng  này đã có, bạn phải nhập mã khác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtMaHang.Focus();
+                txtMaHang.Text = "";
+                return;
+            }
+            sql = "INSERT INTO TblHang(MaHang,TenHang,SoLuong, GiaBan,Anh, GhiChu) VALUES (N'" + txtMaHang.Text.Trim() + "',N'" + txtTenHang.Text.Trim() + "',N'" + txtSoLuong.Text.Trim()  + "',N'" + txtGiaBan.Text.Trim() + "','" + txtAnh.Text + "',N'" + txtGhiChu.Text + "')";
+            Function.RunSQL(sql);
+            LoadDataGridView();
+            ResetValues();
+            btnXoa.Enabled = true;
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnBoQua.Enabled = false;
+            btnLuu.Enabled = false;
+            txtMaHang.Enabled = false;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblHang.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaHang.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtMaHang.Focus();
+                return;
+            }
+            if (txtTenHang.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải nhập tên hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenHang.Focus();
+                return;
+            }
+
+            if (txtAnh.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Bạn phải ảnh minh hoạ cho hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtAnh.Focus();
+                return;
+            }
+            sql = "UPDATE TblHang SET TenHang=N'" + txtTenHang.Text.Trim().ToString() +
+
+                "',SoLuong=" + txtSoLuong.Text +
+                ",Anh='" + txtAnh.Text + "',Ghichu=N'" + txtGhiChu.Text + "' WHERE MaHang=N'" + txtMaHang.Text + "'";
+            Function.RunSQL(sql);
+            LoadDataGridView();
+            ResetValues();
+            btnBoQua.Enabled = false;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            string sql;
+            if (tblHang.Rows.Count == 0)
+            {
+                MessageBox.Show("Không còn dữ liệu!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (txtMaHang.Text == "")
+            {
+                MessageBox.Show("Bạn chưa chọn bản ghi nào", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (MessageBox.Show("Bạn có muốn xoá bản ghi này không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                sql = "DELETE TblHang WHERE MaHang=N'" + txtMaHang.Text + "'";
+                Function.RunSqlDel(sql);
+                LoadDataGridView();
+                ResetValues();
+            }
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            ResetValues();
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnThem.Enabled = true;
+            btnBoQua.Enabled = false;
+            btnLuu.Enabled = false;
+            txtMaHang.Enabled = false;
+        }
+
+
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtMaHang_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void txtTenHang_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void txtSoLuong_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void txtGiaBan_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void txtAnh_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+
+        private void txtGhiChu_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                SendKeys.Send("{TAB}");
+        }
+    }
+}
